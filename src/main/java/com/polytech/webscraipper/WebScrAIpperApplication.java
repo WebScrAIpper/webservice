70540c7accb01faaca787980e606f43697238bd9
noahkohrs <noah@kohrs.fr>
2025-01-28 16:51:30 +0100

    chore: cleaning imports

--- src/main/java/com/polytech/webscraipper/WebScrAIpperApplication.java
+++ src/main/java/com/polytech/webscraipper/WebScrAIpperApplication.java
@@ -1,18 +1,9 @@
 package com.polytech.webscraipper;
 
 import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 
 @SpringBootApplication
 public class WebScrAIpperApplication {
