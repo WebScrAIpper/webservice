d6d5d2adf9eb2857bbeb006c239009af15fae087
Noah Kohrs <noah@kohrs.fr>
2025-01-28 10:18:05 +0100

    first commit

---
+++ src/main/java/com/polytech/webscraipper/WebScrAIpperApplication.java
@@ -0,0 +1,13 @@
+package com.polytech.webscraipper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebScrAIpperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebScrAIpperApplication.class, args);
+    }
+
+}
