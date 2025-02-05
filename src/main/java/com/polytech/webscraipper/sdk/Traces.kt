3917c60806ca884fb81f74fae4a2d51432931054
noahkohrs <noah@kohrs.fr>
2025-02-05 15:17:03 +0100

    chaange

--- src/main/java/com/polytech/webscraipper/sdk/Traces.kt
+++ src/main/java/com/polytech/webscraipper/sdk/Traces.kt
@@ -7,8 +7,6 @@ class Traces(private val tracesClient: TracesClient) {
 
     fun postTrace(traceData: Map<String, Any>): String = tracesClient.postTrace(traceData)
 
-    fun postTrace(vararg entries: Pair<String, Any>): String = postTrace(entries.toMap())
-
     fun postGenericAILog(
         promptName: String,
         promptVersion: Int,
