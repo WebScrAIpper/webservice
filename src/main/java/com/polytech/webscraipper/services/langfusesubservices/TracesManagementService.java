3fdba6e5b78862945b991b6f9c66eb514a556276
noahkohrs <noah@kohrs.fr>
2025-02-07 14:45:22 +0100

    fix: fixing the id extraction and making generic logging easier

--- src/main/java/com/polytech/webscraipper/services/langfusesubservices/TracesManagementService.java
+++ src/main/java/com/polytech/webscraipper/services/langfusesubservices/TracesManagementService.java
@@ -1,62 +1,63 @@
 package com.polytech.webscraipper.services.langfusesubservices;
 
 import com.polytech.webscraipper.sdk.LangfuseSDK;
+import com.polytech.webscraipper.sdk.responses.PromptResponse;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Map;
 
 @Service
 public class TracesManagementService {
 
-    @Autowired
-    private LangfuseSDK langfuseSDK;
+  @Autowired private LangfuseSDK langfuseSDK;
 
-    /**
-     * Post a trace to the Langfuse API
-     *
-     * @return the id of the trace
-     */
-    public int postGenericAILog(
-            String promptName,
-            int promptVersion,
-            String response,
-            String sourceUrl,
-            String sessionId
-            ) {
-        Map<String, Object> traceRequest = Map.of(
-                "name", "Success",
-                "promptName", promptName,
-                "promptVersion", promptVersion,
-                "response", response,
-                "sourceUrl", sourceUrl,
-                "sessionId", sessionId
-                );
-        String res = langfuseSDK.traces.postTrace(traceRequest);
+  /**
+   * Post a trace to the Langfuse API
+   *
+   * @return the id of the trace
+   */
+  public String postGenericAILog(
+      PromptResponse promptResponse, String response, String sourceUrl, String sessionId) {
+    Map<String, Object> traceRequest =
+        Map.of(
+            "name",
+            "Success",
+            "promptName",
+            promptResponse.name,
+            "promptVersion",
+            promptResponse.version,
+            "response",
+            response,
+            "sourceUrl",
+            sourceUrl,
+            "sessionId",
+            sessionId);
+    String res = langfuseSDK.traces.postTrace(traceRequest);
 
-        return extractIdFromResponse(res);
-    }
+    return extractIdFromResponse(res);
+  }
 
-    public int postFailedOutputAILog(
-            String promptName,
-            int promptVersion,
-            String response,
-            String sourceUrl,
-            String sessionId
-            ) {
-        Map<String, Object> traceRequest = Map.of(
-                "name", "Failed",
-                "promptName", promptName,
-                "promptVersion", promptVersion,
-                "response", response,
-                "sourceUrl", sourceUrl,
-                "sessionId", sessionId
-        );
-        String res = langfuseSDK.traces.postTrace(traceRequest);
-        return extractIdFromResponse(res);
-    }
+  public String postFailedOutputAILog(
+      PromptResponse promptResponse, String output, String sourceUrl, String sessionId) {
+    Map<String, Object> traceRequest =
+        Map.of(
+            "name",
+            "Failed",
+            "promptName",
+            promptResponse.name,
+            "promptVersion",
+            promptResponse.version,
+            "response",
+            output,
+            "sourceUrl",
+            sourceUrl,
+            "sessionId",
+            sessionId);
+    String res = langfuseSDK.traces.postTrace(traceRequest);
+    return extractIdFromResponse(res);
+  }
 
-    private int extractIdFromResponse(String res) {
-        return Integer.parseInt(res.substring("{\"id\":\"".length(), res.length() - 2));
-    }
+  private String extractIdFromResponse(String res) {
+    return res.substring("{\"id\":\"".length(), res.length() - 2);
+  }
 }
