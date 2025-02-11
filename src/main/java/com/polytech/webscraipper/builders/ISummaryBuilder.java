115c14753f1a42845d01ae2d0b83b917738b9cbb
noahkohrs <noah@kohrs.fr>
2025-02-11 15:23:43 +0100

    fix: completing the interface

--- src/main/java/com/polytech/webscraipper/builders/ISummaryBuilder.java
+++ src/main/java/com/polytech/webscraipper/builders/ISummaryBuilder.java
@@ -1,18 +1,39 @@
 package com.polytech.webscraipper.builders;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.polytech.webscraipper.PromptException;
 import com.polytech.webscraipper.dto.DocumentDto;
 import com.polytech.webscraipper.sdk.responses.PromptResponse;
+import com.polytech.webscraipper.services.ClassifierService;
+import com.polytech.webscraipper.services.langfusesubservices.PromptManagementService;
+import com.polytech.webscraipper.services.langfusesubservices.TracesManagementService;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface ISummaryBuilder {
+import java.util.List;
+
+@Service
+public abstract class ISummaryBuilder {
+
+  @Autowired protected ChatModel chatModel;
+  @Autowired protected PromptManagementService promptManagementService;
+  @Autowired protected TracesManagementService tracesManagementService;
+
+  protected ObjectMapper objectMapper = new ObjectMapper();
+
+
   // Scrapping the website content
-  public String scrapContent(String pageContent);
+  public abstract String scrapContent(String pageContent);
 
   // Generating the prompt dynamically
-  public PromptResponse generatePrompt(String scrappedContent);
+  public abstract PromptResponse generatePrompt(String scrappedContent, List<String> classifiers);
 
   // Requesting the AI
-  public String requestAI(PromptResponse promptResponse);
+  public abstract String requestAI(PromptResponse promptResponse);
 
   // Polishing the Answer
-  public DocumentDto polishAnswer(String response);
+  public abstract DocumentDto polishAnswer(String url, String response) throws PromptException;
+
+  public abstract boolean isAnAppropriateBuilder(String url);
 }
